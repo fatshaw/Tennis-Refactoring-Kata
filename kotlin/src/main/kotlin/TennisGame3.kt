0916aadb82716b8b64ac60c8f2fa00996885e3b8
chaojie.xiao <chaojie.xiao@airwallex.com>
2019-06-19 16:02:33 +0800

    refactoring kotlin

--- kotlin/src/main/kotlin/TennisGame3.kt
+++ kotlin/src/main/kotlin/TennisGame3.kt
@@ -4,25 +4,46 @@ class TennisGame3(private val p1N: String, private val p2N: String) : TennisGame
     private var p1: Int = 0
 
     override fun getScore(): String {
-        val s: String
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            val p = arrayOf("Love", "Fifteen", "Thirty", "Forty")
-            s = p[p1]
-            return if (p1 == p2) "$s-All" else "$s-${p[p2]}"
-        } else {
-            if (p1 == p2)
-                return "Deuce"
-            s = if (p1 > p2) p1N else p2N
-            return if ((p1 - p2) * (p1 - p2) == 1) "Advantage $s" else "Win for $s"
+        return when {
+            isSameScore() -> return if (isDeuce()) deuce() else sameScore()
+            isNormalScore() -> normalScore()
+            isAdvantage() -> advantageScore()
+            else -> winScore()
         }
     }
 
+    private fun deuce() = "Deuce"
+
+    private fun isNormalScore() = p1 < 4 && p2 < 4
+
+    private fun isAdvantage() = Math.abs(p1 - p2) == 1
+
+    private fun isDeuce() = p1 >= 3 && isSameScore()
+
+    private fun sameScore() = "${SCORE_NAME[p1]}-All"
+
+    private fun normalScore() = "${SCORE_NAME[p1]}-${SCORE_NAME[p2]}"
+
+    private fun winScore() = "Win for ${advPlayer()}"
+
+    private fun advantageScore() = "Advantage ${advPlayer()}"
+
+    private fun advPlayer(): String {
+        return if (p1 > p2) p1N else p2N
+    }
+
+    private fun isSameScore() = p1 == p2
+
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
+        if (playerName === p1N)
             this.p1 += 1
         else
             this.p2 += 1
 
+    }
+
+    companion object {
+        private val SCORE_NAME = arrayOf("Love", "Fifteen", "Thirty", "Forty")
     }
 
 }
